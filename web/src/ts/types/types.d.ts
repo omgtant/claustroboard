2add83ddb25c550af8cde88f3ab8dac9dc327b05
omgaXD <sladekop@gmail.com>
2025-08-13 19:44:46 +0500

    type definitions

---
+++ web/src/ts/types/types.d.ts
@@ -0,0 +1,27 @@
+import { Pos, TileColor } from "./util";
+
+export class GameState {
+    palette: Record<string, {script: string}>[];
+    board: {tile_type: string, data: any}[][];
+    players: Player[];
+}
+
+export class Player {
+    nickname: string;
+    position: Pos;
+}
+
+export class AbstractTile {
+    // Properties
+    position: Pos;
+    is_open: boolean;
+    color: TileColor;
+
+    // Hooks
+    onTurnStart(state: GameState): void;
+    availableMoves(state: GameState, player: Player): Pos[];
+    onPlayerLanding(state: GameState, player: Player): void;
+
+    canLandOnMe(state: GameState, player: Player): boolean;
+    canStartOnMe(state: GameState, player: Player): boolean;
+}
---
+++ web/src/ts/types/util.d.ts
@@ -0,0 +1,9 @@
+export type Pos = {x: number, y: number};
+
+export enum TileColor {
+    COLORLESS = 0,
+    RED = 1,
+    YELLOW = 2,
+    GREEN = 3,
+    BLUE = 4
+}
