56265c36ea723a2778b98973770a2a419f5e4cb8
omgaXD <sladekop@gmail.com>
2025-08-13 14:36:48 +0500

    add frontend dev pipeline

--- web/main.go
+++ web/main.go
@@ -1,24 +1,38 @@
 package web
 
 import (
-	"embed"
-	"io/fs"
-	"net/http"
+    "embed"
+    "io/fs"
+    "net/http"
+    "os"
+    "path/filepath"
+    "runtime"
 )
 
 var (
-	//go:embed out
-	StaticFiles embed.FS
+    //go:embed out
+    StaticFiles embed.FS
 )
 
 func GetRouter() *http.ServeMux {
-	mux := http.NewServeMux()
+    mux := http.NewServeMux()
 
-	staticFS, err := fs.Sub(StaticFiles, "out")
-	if err == nil {
-		fs := http.FileServer(http.FS(staticFS))
-		mux.Handle("/", fs)
-	}
+    // Prefer serving from disk during development
+    _, thisFile, _, _ := runtime.Caller(0)
+    projectRoot := filepath.Clean(filepath.Join(filepath.Dir(thisFile), ".."))
+    diskOut := filepath.Join(projectRoot, "web", "out")
 
-	return mux
+    if fi, err := os.Stat(diskOut); err == nil && fi.IsDir() {
+        mux.Handle("/", http.FileServer(http.Dir(diskOut)))
+        return mux
+    }
+
+    // Fallback to embedded assets (production)
+    staticFS, err := fs.Sub(StaticFiles, "out")
+    if err == nil {
+        fs := http.FileServer(http.FS(staticFS))
+        mux.Handle("/", fs)
+    }
+
+    return mux
 }
