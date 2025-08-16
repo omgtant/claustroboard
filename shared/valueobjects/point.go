f9b4c37db8e1829bfb4abddcce928e4a0a5568d1
Miltant <baptistemarsigli@gmail.com>
2025-08-16 13:36:23 +0200

    add concurrency control to Board and improve move validation logic

--- shared/valueobjects/point.go
+++ shared/valueobjects/point.go
@@ -1,6 +1,12 @@
 package valueobjects
 
+import "fmt"
+
 type Point struct {
 	X uint16 `json:"x"`
 	Y uint16 `json:"y"`
+}
+
+func (p Point) String() string {
+	return fmt.Sprintf("(%d,%d)", p.X, p.Y)
 }
