4bb11bf9628646b4381e11884c31ff028eb1de4d
Miltant <baptistemarsigli@gmail.com>
2025-08-16 15:14:22 +0200

    increase ping timeout

--- web/routers/ws.go
+++ web/routers/ws.go
@@ -98,7 +98,7 @@ func (c *wsClient) writePing() {
 	for {
 		select {
 		case <-t.C:
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
 			_ = c.conn.Ping(ctx)
 			cancel()
 		}
