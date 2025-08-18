029acb764fdbd87ab319e04c99522fc4984bba2d
omgaXD <sladekop@gmail.com>
2025-08-18 15:15:11 +0500

    replace map of templates with a single pointer to template

--- web/routers/template.go
+++ web/routers/template.go
@@ -2,45 +2,37 @@ package routers
 
 import (
 	"embed"
-	"fmt"
 	"html/template"
 	"net/http"
 )
 
-var templates map[string]*template.Template
+var templates *template.Template
 
 func InitTemplates(fs embed.FS) error {
-	templates = make(map[string]*template.Template)
-
-	tmpl, err := template.ParseFS(fs, "out/index.html", "out/*.html")
+	var err error
+	templates, err = template.ParseFS(fs, "out/*.html")
 	if err != nil {
 		return err
 	}
 
-	templates["default"] = tmpl
 	return nil
 }
 
 func RenderTemplate(w http.ResponseWriter, name string, data any) error {
-	tmpl, ok := templates[name]
-	if !ok {
-		return fmt.Errorf("template not found: %s", name)
-	}
-
-	return tmpl.Execute(w, data)
+	return templates.ExecuteTemplate(w, name, data)
 }
 
 // TemplateHandler returns an http.HandlerFunc that renders a template with the provided name and data function.
 // dataFunc is a function that takes an *http.Request and returns the data to be passed to the template.
 func TemplateHandler(name string, dataFunc func(*http.Request) any) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        var data any
-        if dataFunc != nil {
-            data = dataFunc(r)
-        }
-        
-        if err := RenderTemplate(w, name, data); err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		var data any
+		if dataFunc != nil {
+			data = dataFunc(r)
+		}
+
+		if err := RenderTemplate(w, name, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
 }
